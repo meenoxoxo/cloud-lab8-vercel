d21102a49685793715223c5f5d221c499a63f49a
meenoxoxo <zkkk12309@gmai.com>
2026-05-23 00:42:57 +0500

    Lab 8 fresh init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+ return (
+ <main
+style={{textAlign:"center",
+ marginTop:"10%"}}>
+ <h1>Lab 8 — Vercel
+Deployment</h1>
+ <p>Name: [Zarmeena Khan]</p>
+ <p>Roll: [01-131232-096]</p>
+ </main>
+ );
+}
